e56c1c0a218c9d5bee92b290d3b2503213811bd7
Jonathan <jonathancamiling@gmail.com>
2022-05-07 23:06:05 -0700

    refactored for getter and setter

--- index.js
+++ index.js
@@ -19,7 +19,13 @@ class Timer {
     clearInterval(this.interval);
   };
   tick = () => {
-    console.log('tick')
+    this.timeRemaining = this.timeRemaining - 1; 
+  }
+  get timeRemaining(){
+    return parseFloat(this.durationInput.value)
+  }
+  set timeRemaining(time){
+    this.durationInput.value = time; 
   }
   
 }
