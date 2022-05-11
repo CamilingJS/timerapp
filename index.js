7993bd4fcfb22e412b2362d8fa982eaa8e432db2
CamilingJS <JCamiling@employersadvantagedirect.com>
2022-05-11 09:42:11 -0700

    added callbacks for onStart() onTick() and onComplete()

--- index.js
+++ index.js
@@ -9,6 +9,8 @@ class Timer {
     this.pauseButton = pauseButton;
     if(callbacks){
       this.onStart = callbacks.onStart; 
+      this.onTick = callbacks.onTick; 
+      this.onComplete = callbacks.onComplete; 
     }
 
     this.startButton.addEventListener('click', this.start);
@@ -26,9 +28,15 @@ class Timer {
   };
   tick = () => {
     if (this.timeRemaining <= 0){
-      this.pause()
+      this.pause();
+      if (this.onComplete){
+        this.onComplete(); 
+      }
     } else{
       this.timeRemaining = this.timeRemaining - 1; 
+      if (this.onTick) {
+        this.onTick(); 
+      }
     }
 
     
@@ -47,10 +55,10 @@ const timer = new Timer(durationInput, startButton, pauseButton, {
     console.log("Timer started")
   },
   onTick(){
-
+    console.log("Timer just ticked down")
   }, 
   onComplete(){
-
+    console.log("Timer is completed")
   }
 })
 
